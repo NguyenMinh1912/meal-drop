b2fb585f18307276c8449234bdeab2c709b74403
Nguyễn Minh <76939717+NguyenMinh1912@users.noreply.github.com>
2024-10-07 21:59:57 +0700

    add lost pixel config

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -1,14 +1,8 @@
-import { CustomProjectConfig } from 'lost-pixel';
+import type { CustomProjectConfig } from 'lost-pixel';
 
 export const config: CustomProjectConfig = {
   storybookShots: {
-    storybookUrl: './storybook-static',
+    storybookUrl: 'examples/storybook-build/storybook-static',
   },
-   // OSS mode 
   generateOnly: true,
-  failOnDifference: true
-  
-  // Lost Pixel Platform (to use in Platform mode, comment out the OSS mode and uncomment this part )
-  // lostPixelProjectId: "xxxx",
-  // process.env.LOST_PIXEL_API_KEY,
 };
